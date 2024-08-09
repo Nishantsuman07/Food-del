25a25c4ed6d28a2c58a3b3b23bb8aeeb0ccc4985
Nishantsuman07 <134281310+Nishantsuman07@users.noreply.github.com>
2024-08-09 17:14:35 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,8 +10,7 @@ const Footer = () => {
             <img src={assets.logo} alt="" />
             <p>TOMATO is a project created with passion and dedication by Nishant, a BTech 4th year student at DTU College. This food ordering platform was built to provide users with a seamless and enjoyable experience, combining the power of modern web technologies. Special thanks to the DTU community and all those who supported the development of this website. Your feedback and support are highly valued as we continue to improve and grow TOMATO. Thank you for being a part of this journey.</p>
                 <div className="footer-social-icons">
-                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                <img src={assets.facebook_icon} alt="Facebook" />
+                <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
                 <img src={assets.linkedin_icon} alt="" />
             </div>
